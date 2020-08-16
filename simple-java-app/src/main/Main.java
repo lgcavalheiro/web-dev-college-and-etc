b748123d0605dee3eceda2ef04aa38883796f149
lgcavalheiro <lg.cavalheiro@hotmail.com>
2020-08-16 22:17:30 +0000

    make manifest work but it has to be inside classes folder alongside makefile

--- simple-java-app/src/main/Main.java
+++ simple-java-app/src/main/Main.java
@@ -3,5 +3,6 @@ package main;
 public class Main {
     public static void main(final String[] args) {
         System.out.println("Hello Java World without IDE!");
+        System.out.println("Some more helloes!");
     }
 }
