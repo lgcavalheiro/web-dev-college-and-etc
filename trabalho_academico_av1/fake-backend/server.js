55d637473001172f4189a616bdd3cab3ee122b7b
Lucas Cavalheiro <lg.cavalheiro@hotmail.com>
2020-09-14 01:46:06 -0300

    added navigation checks

--- trabalho_academico_av1/fake-backend/server.js
+++ trabalho_academico_av1/fake-backend/server.js
@@ -11,15 +11,14 @@ server.use(middlewares)
 server.use(jsonServer.bodyParser)
 
 server.post('/login', (req, res) => {
-    if(req.body.password && req.body.id){
+    if (req.body.password && req.body.id) {
         let isAuth = auth.authenticate(req.body.id, req.body.password);
-        if(isAuth){
-            res.status(200).jsonp({ role: isAuth.role, name: isAuth.name, id: isAuth.id })
-        } else
-            res.status(401).jsonp({ error: "Id e/ou senha não conferem, verifique suas credenciais e tente novamente."}) 
-    } else {
-        res.status(401).jsonp({ error: "Credenciais inválidas."}) 
-    }
+        if (isAuth) res.status(200).jsonp({ role: isAuth.role, name: isAuth.name, id: isAuth.id })
+        else res.status(401).jsonp(
+            { error: "Id e/ou senha não conferem, verifique suas credenciais e tente novamente." }
+        )
+    } else
+        res.status(401).jsonp({ error: "Credenciais inválidas ou incompletas." })
 })
 
 server.use(router)
