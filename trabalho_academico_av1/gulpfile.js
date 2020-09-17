dc039a4568e1f3844f0fbe85a349ffed0a07f7da
Lucas Cavalheiro <lg.cavalheiro@hotmail.com>
2020-09-17 02:28:40 -0300

    improved gulp workflow with parcel and it is pretty cool bro

--- trabalho_academico_av1/gulpfile.js
+++ trabalho_academico_av1/gulpfile.js
@@ -2,51 +2,65 @@ const { exec } = require('child_process');
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
-const uglifyCss =  require('gulp-uglifycss');
-const terser = require('gulp-terser');
-const babel = require('gulp-babel');
+const uglifyCss = require('gulp-uglifycss');
+const replace = require('gulp-replace');
+/* const terser = require('gulp-terser');
+const babel = require('gulp-babel'); */
 const series = gulp.series;
 const parallel = gulp.parallel;
 
-function purgePublic(){
+function purgeFolders() {
     return exec('yarn purge', (err, stdout, stderr) => {
         if (err) {
-          console.log('Error: ', err);
-          return;
+            console.log('Error: ', err);
+            return;
         }
         console.log(`stdout: ${stdout}`);
         console.log(`stderr: ${stderr}`);
-      });
+    });
+}
+
+function runParcel() {
+    return exec('yarn build', (err, stdout, stderr) => {
+        if (err) {
+            console.log('Error: ', err);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+    });
 }
 
 //gulp sucks at js
 function processJs() {
-    return gulp.src('./public/*.js')
+    return gulp.src('./parceled/*.js')
         //.pipe(babel({ comments: false, presets: ["@babel/preset-env"] }))
         //.pipe(terser())
         //.pipe(concat('index.js'))
-        .pipe(gulp.dest('./public2'))
+        .pipe(gulp.dest('./public'))
 }
 
 function processSass() {
-    return gulp.src('./public/*.css')
+    return gulp.src('./parceled/*.css')
         .pipe(sass().on('error', sass.logError))
         .pipe(uglifyCss({ "uglyComments": true }))
         .pipe(concat('index.css'))
-        .pipe(gulp.dest('./public2'))
+        .pipe(gulp.dest('./public'))
 }
 
-function copyHtml() {
-    return gulp.src('./public/*.html')
-        .pipe(gulp.dest('./public2'))
+function processHtml() {
+    return gulp.src('./parceled/*.html')
+        .pipe(replace(/href=\"\/(?!favicon)[a-zA-Z0-9-.]+\"/g, 'href="index.css"'))
+        .pipe(gulp.dest('./public'))
 }
 
 function copyAssets() {
-    return gulp.src('./public/*.ico')
-        .pipe(gulp.dest('./public2'))
+    return gulp.src('./parceled/*.ico')
+        .pipe(gulp.dest('./public'))
 }
 
 exports.default = series(
-        //purgePublic,
-        parallel(processSass, copyHtml, processJs, copyAssets)
-    )
+    purgeFolders,
+    runParcel,
+    parallel(processSass, processHtml, processJs, copyAssets)
+)
