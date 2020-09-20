01011131ce5531b3a578f4640b296c37e281f623
Lucas Cavalheiro <lg.cavalheiro@hotmail.com>
2020-09-20 12:44:06 -0300

    making some css modifications

--- trabalho_academico_av1/frontend/js/app-index.js
+++ trabalho_academico_av1/frontend/js/app-index.js
@@ -1,4 +1,4 @@
-document.loginForm.onsubmit = async (e) => {
+document.loginForm.onsubmit = async(e) => {
     e.preventDefault();
     const data = new FormData(e.target);
     const options = {
@@ -9,8 +9,8 @@ document.loginForm.onsubmit = async (e) => {
     fetch(document.loginForm.action, options)
         .then(r => r.json())
         .then(res => {
-            if(res.error) throw Error(res.error)
-            if(res.role === 'professor') window.location.assign('/home-professor.html')
+            if (res.error) throw Error(res.error)
+            if (res.role === 'professor') window.location.assign('/home-professor.html')
             else window.location.assign('/home-aluno.html')
             localStorage.setItem("role", res.role);
             localStorage.setItem("name", res.name);
