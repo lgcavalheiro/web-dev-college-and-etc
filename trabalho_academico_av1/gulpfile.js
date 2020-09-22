ffaa10a74e58ef30ef8e9c959092732197c86d21
Lucas Cavalheiro <lg.cavalheiro@hotmail.com>
2020-09-21 23:47:13 -0300

    added gulp back in for building everything in a single command, works nicely now and is debloated

---
+++ trabalho_academico_av1/gulpfile.js
@@ -0,0 +1,37 @@
+const gulp = require('gulp');
+const watch = gulp.watch;
+const series = gulp.series;
+const { exec, spawn } = require('child_process');
+var serverProc = undefined;
+
+const watcher = watch(['./frontend/**/*', './backend/**/*', '!./backend/**/*.json']);
+
+watcher.on('change', function (path, stats) {
+    console.log(`File ${path} was changed - Relaunching...`);
+    serverProc.kill('SIGINT');
+    exports.default();
+});
+
+watcher.on('error', function (e) {
+    console.error(e.stack);
+});
+
+function purge() {
+    return exec('shx rm -rf ./public/*');
+}
+
+function bundle() {
+    return exec('parcel build ./frontend/html/*.html --out-dir public --no-source-maps');
+}
+
+function serve() {
+    if (serverProc) serverProc.kill('SIGINT');
+    serverProc = spawn('node', ['backend/server.js'], { stdio: 'inherit' });
+    serverProc.on('close', function (code) {
+        if (code === 8) {
+            gulp.log('Error detected, waiting for changes...');
+        }
+    });
+}
+
+exports.default = series(purge, bundle, serve);
