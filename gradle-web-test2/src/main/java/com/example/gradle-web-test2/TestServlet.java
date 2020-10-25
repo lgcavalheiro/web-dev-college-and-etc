d5c6313e2130dc3f6b475e8484c8e79c75251b77
Lucas Cavalheiro <lg.cavalheiro@hotmail.com>
2020-10-25 18:57:37 -0300

    setting up java vscodium extensions

--- gradle-web-test2/src/main/java/com/example/gradle-web-test2/TestServlet.java
+++ gradle-web-test2/src/main/java/com/example/gradle-web-test2/TestServlet.java
@@ -8,20 +8,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class TestServlet extends HttpServlet {
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            int num = Integer.parseInt(request.getParameter("numero"));
-            int fib = fibonacci(num);
-            System.out.println(fib);
-            out.println("<h1>" + fib + "</h1>");
-        }
-    }
 
-    public int fibonacci(int n) {
-        if(n == 0 || n == 1) return 1;
-        else return fibonacci(n - 1) + fibonacci(n - 2);
+  @Override
+  protected void doPost(
+    HttpServletRequest request,
+    HttpServletResponse response
+  )
+    throws IOException {
+    response.setContentType("text/html;charset=UTF-8");
+    try (PrintWriter out = response.getWriter()) {
+      int num = Integer.parseInt(request.getParameter("numero"));
+      int fib = fibonacci(num);
+      System.out.println(fib);
+      out.println("<h1>" + fib + "</h1>");
     }
+  }
+
+  public int fibonacci(int n) {
+    if (n == 0 || n == 1) return 1; else return (
+      fibonacci(n - 1) + fibonacci(n - 2)
+    );
+  }
 }
